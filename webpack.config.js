7bd6c659265ec13eed358c8efaab6d6c015588ac
shanejonas <jonas.shane@gmail.com>
2021-06-30 15:33:56 -0700

    changed webpack config to handle prod vs dev

--- webpack.config.js
+++ webpack.config.js
@@ -11,45 +11,54 @@ const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 // eslint-disable-next-line import/no-dynamic-require
 const prefix = require(resolveApp('package.json')).homepage;
 
-// eslint-disable-next-line node/no-process-env
-const isProd = process.env.NODE_ENV === 'production';
-const htmlwebpackOptions = isProd ? { publicPath: prefix } : undefined;
+module.exports = (env, argv) => {
+  // eslint-disable-next-line node/no-process-env
+  const isProd = argv.mode === 'production';
+  const htmlwebpackOptions = isProd ? { publicPath: prefix } : undefined;
 
-module.exports = {
-  devtool: 'inline-source-map',
-  mode: 'development',
-  entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: DIST,
-    publicPath: DIST,
-  },
-  devServer: {
-    contentBase: DIST,
-    port: 9011,
-    writeToDisk: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin(htmlwebpackOptions),
-    new NodePolyfillPlugin({
-      excludeAliases: ['stream', 'buffer'],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/u,
-        use: 'ts-loader',
-        exclude: /node_modules/u,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      stream: require.resolve('stream-browserify'),
-      // eslint-disable-next-line node/no-extraneous-require
-      buffer: require.resolve('buffer-browserify'),
+  let extraOptions = {};
+
+  if (isProd === false) {
+    extraOptions = {
+      devtool: 'inline-source-map',
+    };
+  }
+  const config = {
+    ...extraOptions,
+    entry: './src/index.ts',
+    output: {
+      filename: 'bundle.js',
+      path: DIST,
+      publicPath: DIST,
     },
-  },
-};
+    devServer: {
+      contentBase: DIST,
+      port: 9011,
+      writeToDisk: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin(htmlwebpackOptions),
+      new NodePolyfillPlugin({
+        excludeAliases: ['stream', 'buffer'],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/u,
+          use: 'ts-loader',
+          exclude: /node_modules/u,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      fallback: {
+        stream: require.resolve('stream-browserify'),
+        // eslint-disable-next-line node/no-extraneous-require
+        buffer: require.resolve('buffer-browserify'),
+      },
+    },
+  };
+  return config;
+}
