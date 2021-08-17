624c9c851bcf7565ff2391e17bb6dd01686da747
Shane <jonas.shane@gmail.com>
2021-08-17 14:23:41 -0700

    Remove stream name override for metamask provider (#17)

--- src/controller.ts
+++ src/controller.ts
@@ -194,7 +194,6 @@ class Controller {
    */
   private createPluginProvider(pluginName: string): PluginProvider {
     const pluginProvider = (new MetaMaskInpageProvider(this.rpcStream as any, {
-      jsonRpcStreamName: pluginName,
       shouldSendMetadata: false,
     }) as unknown) as Partial<PluginProvider>;
 
