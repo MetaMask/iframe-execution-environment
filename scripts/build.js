d4c4fe8cf31ad459fe85c0a7029dbb09c733cfd2
Frederik Bolding <frederik.bolding@gmail.com>
2023-02-22 16:39:06 +0100

    `snaps-execution-environments@0.30.0` (#137)

--- scripts/build.js
+++ scripts/build.js
@@ -8,14 +8,13 @@ const { PUBLIC, ROOT } = require('./common');
 const NODE_MODULES = path.join(ROOT, 'node_modules');
 
 const HTML_FILE_NAME = 'index.html';
-const LOCKDOWN_FILE_NAME = 'lockdown.umd.min.js';
 
 main();
 
 /**
  * Our build process for the iframe execution environment website. In detail:
  * - Deletes and rewrites the destination directory, `/public`.
- * - Copies `index.html` and our JavaScript bundles to the destination directory.
+ * - Copies `index.html` and our JavaScript bundle to the destination directory.
  */
 async function main() {
   await rimraf(PUBLIC);
@@ -28,15 +27,6 @@ async function main() {
       '@metamask/snaps-execution-environments/dist/webpack/iframe/index.html',
     ),
     path.join(PUBLIC, HTML_FILE_NAME),
-  );
-
-  // public/lockdown.umd.min.js
-  await copyFile(
-    path.join(
-      NODE_MODULES,
-      `@metamask/snaps-execution-environments/dist/webpack/iframe/${LOCKDOWN_FILE_NAME}`,
-    ),
-    path.join(PUBLIC, LOCKDOWN_FILE_NAME),
   );
 
   // public/bundle.js
